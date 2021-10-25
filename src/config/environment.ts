e24884da99c82b0c75f73df6f4a32a47507b1065
beetlejuice96 <dilanbernbae043@gmail.com>
2021-10-24 21:32:36 -0300

    add new connection and configuration with database postgresql

---
+++ src/config/environment.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const environment = {
+  PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-import {startServer} from  './server';
+import { startServer } from "./server";
 
-const main = async() => {
-    const port:number=4000;
-    const app = await startServer();
-    app.listen(port);
-    console.log(`app running on port ${port}` )
-}
+import { connect } from "./config/typeorm";
+
+const main = async () => {
+  try {
+    connect();
+  } catch (error) {
+    console.log(error);
+  }
+
+  const port: number = 4000;
+  const app = await startServer();
+  app.listen(port);
+  console.log(`app running on port ${port}`);
+};
 
 main();
