7d5b15595601d348e2f7e632a274135eabd123fe
beetlejuice96 <dilanbernbae043@gmail.com>
2021-11-20 21:30:38 -0300

    add more functions to crud authors

--- src/resolvers/author.resolver.ts
+++ src/resolvers/author.resolver.ts
@@ -1,10 +1,33 @@
-import { Mutation, Arg, Resolver, InputType, Field } from "type-graphql";
+import {
+  Mutation,
+  Arg,
+  Resolver,
+  InputType,
+  Field,
+  Query,
+  Args,
+} from "type-graphql";
 import { Author } from "../entity/author.entity";
 import { getRepository, Repository } from "typeorm";
 @InputType()
 class AuthorInput {
   @Field()
   fullName!: string;
+}
+
+@InputType()
+class AuthorUpdateInput {
+  @Field(() => Number)
+  id!: number;
+
+  @Field()
+  fullName?: string;
+}
+
+@InputType()
+class AuthorIdInput {
+  @Field(() => Number)
+  id!: number;
 }
 
 @Resolver()
@@ -30,5 +53,56 @@ export class AuthorResolver {
     } catch {
       console.error;
     }
+  }
+
+  @Query(() => [Author]) // con los brackets le estamos indicando que vamos a obtener un array
+  async getAllAuthors(): Promise<Author[]> {
+    return await this.authorRepository.find();
+  }
+
+  @Query(() => Author)
+  async getOneAuthor(
+    @Arg("input", () => AuthorIdInput) input: AuthorIdInput
+  ): Promise<Author | undefined> {
+    try {
+      const author = await this.authorRepository.findOne(input.id);
+      if (!author) {
+        const error = new Error();
+        error.message = "author does not exist";
+        throw error;
+      }
+      return author;
+    } catch (e) {
+      throw new Error(e as string);
+    }
+  }
+
+  @Mutation(() => Author)
+  async updateAuthor(
+    @Arg("input", () => AuthorUpdateInput) input: AuthorUpdateInput
+  ): Promise<Author | undefined> {
+    const authorExist = await this.authorRepository.findOne(input.id);
+
+    if (!authorExist) {
+      throw new Error("Author does not exist");
+    }
+
+    return await this.authorRepository.save({
+      id: input.id,
+      fullName: input.fullName,
+    });
+  }
+
+  @Mutation(() => Boolean)
+  async deleteOneAuthor(
+    @Arg("input", () => AuthorIdInput) input: AuthorIdInput
+  ): Promise<Boolean> {
+    // const authorExist = await this.authorRepository.findOne(input.id);
+
+    // if (!authorExist) {
+    //   throw new Error("Author does not exist");
+    // }
+    await this.authorRepository.delete(input.id);
+    return true;
   }
 }
